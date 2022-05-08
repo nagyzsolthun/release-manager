68dab895807bef6345d615f27a4a38e21d979546
nagyzsolthun <nagy.zsolt.hun@gmail.com>
2022-05-08 14:23:31 +0200

    spring initializr

---
+++ src/main/java/hu/zsoltn/releasemanager/ReleaseManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.zsoltn.releasemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/zsoltn/releasemanager/ReleaseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zsoltn.releasemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
